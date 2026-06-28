da82fa086cd7dac0b476c1d7c573e8dba021f7cf
Ángel Criado Bertoli <angelcriber99@gmail.com>
2026-06-29 01:09:47 +0200

    Remove test scripts

--- check_pg_views.ts
+++
@@ -1,1 +0,0 @@
-import { createClient } from '@supabase/supabase-js'; import * as dotenv from 'dotenv'; dotenv.config({ path: '.env.local' }); const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY); supabase.from('posiciones').select('*').then(console.log);
