48d866ef67b03e49729ca575d39715ac9a7bcebe
Ángel Criado Bertoli <angelcriber99@gmail.com>
2026-07-20 19:04:16 +0200

    chore: remove test scripts causing build failure

--- test-yahoo.ts
+++
@@ -1,9 +0,0 @@
-import yahooFinance from 'yahoo-finance2'
-
-async function test() {
-  const chart = await yahooFinance.chart('ASTS', { interval: '1m', period1: new Date(Date.now() - 24 * 60 * 60 * 1000), includePrePost: true })
-  console.log('chartPreviousClose:', chart.meta.chartPreviousClose)
-  console.log('previousClose:', chart.meta.previousClose)
-  console.log('regularMarketPrice:', chart.meta.regularMarketPrice)
-}
-test().catch(console.error)
