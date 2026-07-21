1b54f6f4b204a8d99a065ba02bc7c60e14d25b0f
Ángel Criado Bertoli <angelcriber99@gmail.com>
2026-07-21 12:55:17 +0200

    Remove test_yahoo.ts to fix build

--- test_yahoo.ts
+++
@@ -1,7 +0,0 @@
-import { fetchPrices } from './lib/actions/market';
-
-async function main() {
-  const res = await fetchPrices(['ASTS', 'GRRR', 'UNH', 'BABA', 'ADUR']);
-  console.log(JSON.stringify(res, null, 2));
-}
-main();
